95f51c66bfe45fc29a0fb5788f89c40ccd8e5aec
Orian <77785778+OrianP@users.noreply.github.com>
2021-05-10 17:52:16 +0100

    Music player add js functionality

---
+++ script.js
@@ -0,0 +1,101 @@
+// Music Player
+
+const musicContainer = document.querySelector(".music-container");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
+const title = document.querySelector("#song-title");
+const cover = document.querySelector("#cover");
+
+// song titles
+const songs = ["Here We Are", "Dancing On The Floor", "Leave It Til Tomorrow", "When It Comes To Love"];
+
+const colors = ["#f68e88", "#006184", "#1a4d31", "#930118"];
+
+let songIndex = 0;
+
+// load song info to DOM
+loadSong(songs[songIndex]);
+
+// load and update song title and media
+function loadSong(song) {
+    title.textContent = song;
+    audio.src = `media/music/${song}.mp3`;
+    cover.src = `media/images/${song}.jpg`;
+}
+
+// functions for play and pause btns
+function playSong() {
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+    audio.play();
+};
+
+function pauseSong() {
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+    audio.pause();
+}
+
+// functions for prev and next btns
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function updateProgress (e) {
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+}
+
+function setProgress (e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+// event listeners
+// play and pause btns
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains("play");
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+})
+
+// prev and next btns
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// progress bar 
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
+
+// play next song when current song ends
+audio.addEventListener('ended', nextSong);
+
+
+
